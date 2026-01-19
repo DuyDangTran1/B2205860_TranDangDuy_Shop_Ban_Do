55dae412fd7341c756f9fd970c7b3a2f0b36c968
DangDuyTran1 <tuechst1@gmail.com>
2026-01-19 15:21:31 +0700

    Chinh sua router cua san pham

--- shop_backend/app/routes/product.route.js
+++ shop_backend/app/routes/product.route.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const products = require("../Controllers/products.controller");
 const router = express.Router();
+const Upload = require("../middelwares/multer.middelware");
 
-router.route("/").post(products.create);
-router.route("/:category").get(products.findAllByCategory);
 router
-  .route("/:id")
-  .get(products.findOneById)
-  .put(products.update)
+  .route("/")
+  .post(Upload.createUpload("products").single("image"), products.create);
+
+router.route("/:category_name").get(products.findAllByCategory);
+
+router
+  .route("/detail/:id")
+  .get(products.getProductById)
+  .put(Upload.createUpload("products").single("image"), products.update)
   .delete(products.delete);
 
 module.exports = router;
