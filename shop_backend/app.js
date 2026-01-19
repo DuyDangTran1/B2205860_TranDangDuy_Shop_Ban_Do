348b92099575156b1ccbd6d0b8493142c5e92f3d
DangDuyTran1 <tuechst1@gmail.com>
2026-01-19 15:24:00 +0700

    Cap nhat cau hinh ung dung

--- shop_backend/app.js
+++ shop_backend/app.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-const productsRouter = require("./app/routes/product.route");
+const ProductsRouter = require("./app/routes/product.route");
+const ProductVariantRouter = require("./app/routes/product_variant.route");
+const CategoriesRouter = require("./app/routes/categories.route");
+const ColorRouter = require("./app/routes/color.route");
+const SizeRouter = require("./app/routes/size.route");
+const CollectionRouter = require("./app/routes/collection.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/products", productsRouter);
+app.use(express.static("public"));
+app.use("/api/products", ProductsRouter);
+app.use("/api/product_variant", ProductVariantRouter);
+app.use("/api/category", CategoriesRouter);
+app.use("/api/color", ColorRouter);
+app.use("/api/size", SizeRouter);
+app.use("/api/collection", CollectionRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Không tìm thấy tài nguyên"));
+});
+
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Lỗi server",
+  });
+});
 module.exports = app;
--- shop_backend/server.js
+++ shop_backend/server.js
@@ -1,7 +1,19 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log("Đã kết nối đến cơ sở dữ liệu!");
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server đã chạy trên cổng ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Không thể kết nối đến database", error);
+    process.exit();
+  }
+}
+
+startServer();
