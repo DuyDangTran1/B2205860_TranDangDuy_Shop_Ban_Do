ab029f0423991c51dc46837284a86b197d99bd25
DangDuyTran1 <tuechst1@gmail.com>
2026-03-20 11:59:45 +0700

    File chua phan loading

---
+++ shop_frontend/src/components/Loading.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="isLoading" class="loading-overlay">
+    <div class="spinner-border text-brown" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["isLoading"],
+};
+</script>
+
+<style scoped>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(255, 255, 255, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+
+.text-brown {
+  color: #533422;
+}
+
+.spinner-border {
+  width: 3.5rem;
+  height: 3.5rem;
+  border-width: 0.25em;
+}
+</style>
