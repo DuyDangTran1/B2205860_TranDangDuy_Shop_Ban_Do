80c1f01de06fb7d5086eeeb484af90e71c3e1df7
DangDuyTran1 <tuechst1@gmail.com>
2026-03-20 11:37:37 +0700

    Them cac api xu ly the loai

--- shop_backend/app/routes/categories.route.js
+++ shop_backend/app/routes/categories.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.route("/").post(categories.create).get(categories.getAllCategories);
 
+router.route("/tree/:slug").get(categories.getCategoriesTree);
 router.route("/:id").put(categories.update).delete(categories.delete);
-
 module.exports = router;
