3d12a1d36d5135f353cf1b346d796b4bdd4811e4
DangDuyTran1 <tuechst1@gmail.com>
2026-03-20 12:13:31 +0700

    Mot so cap nhat moi

--- shop_backend/app/services/categories.service.js
+++ shop_backend/app/services/categories.service.js
@@ -24,8 +24,21 @@ class Categories {
     return await this.Categories.findOneAndUpdate(
       { category_name: category.category_name },
       { $set: category },
-      { returnDocument: "after", upsert: true }
+      { returnDocument: "after", upsert: true },
     );
+  }
+
+  //hàm chuyển chữ tiếng viết về không dấu
+  toSlug(str) {
+    return str
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[đĐ]/g, "d")
+      .replace(/([^0-9a-z-\s])/g, "")
+      .replace(/(\s+)/g, "-")
+      .replace(/-+/g, "-")
+      .replace(/^-+|-+$/g, "");
   }
 
   //Hàm cập nhật thông tin của 1 thể loại
@@ -39,7 +52,7 @@ class Categories {
     return await this.Categories.findOneAndUpdate(
       filter,
       { $set: category },
-      { returnDocument: "after" }
+      { returnDocument: "after" },
     );
   }
 
@@ -54,8 +67,67 @@ class Categories {
     return this.Categories.find().toArray();
   }
 
-  async findCategoryByName(category_name) {
-    return await this.Categories.findOne({ category_name: category_name });
+  async findCategoryBySlug(slug) {
+    return await this.Categories.findOne({ slug: slug });
+  }
+
+  async getRootAncestor(slug) {
+    const result = await this.Categories.aggregate([
+      { $match: { slug: slug } },
+
+      {
+        $graphLookup: {
+          from: "Categories",
+          startWith: "$parent_name",
+          connectFromField: "parent_name",
+          connectToField: "category_name",
+          as: "familyTree",
+        },
+      },
+
+      {
+        $project: {
+          root_name: {
+            $filter: {
+              input: "$familyTree",
+              as: "item",
+              cond: {
+                $or: [
+                  { $eq: ["$$item.parent_name", null] },
+                  { $eq: ["$$item.parent_name", ""] },
+                ],
+              },
+            },
+          },
+        },
+      },
+    ]).next();
+
+    if (!result || result.root_name.length === 0) {
+      const self = await this.Categories.findOne({
+        slug: slug,
+      });
+      return self?.parent_name ? null : currentCategoryName;
+    }
+
+    return result.root_name[0].category_name;
+  }
+
+  async getCategoryTree(category_name) {
+    return this.Categories.aggregate([
+      {
+        $match: { category_name: category_name },
+      },
+      {
+        $graphLookup: {
+          from: "Categories",
+          startWith: "$category_name",
+          connectFromField: "category_name",
+          connectToField: "parent_name",
+          as: "Tree",
+        },
+      },
+    ]).toArray();
   }
 }
 
--- shop_backend/app/utils/mongodb.util.js
+++ shop_backend/app/utils/mongodb.util.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require("mongodb");
-
+const SessionService = require("../services/session.service");
 class MongoDB {
   static connect = async (uri) => {
     if (this.client) return this.client;
     this.client = await MongoClient.connect(uri);
+
+    const Session = new SessionService(this.client);
+    await Session.initIndex();
     return this.client;
   };
 }
