66b0685709596afaa5af54471177f01e020e4f97
DangDuyTran1 <tuechst1@gmail.com>
2026-03-20 11:37:07 +0700

    Cap nhat file cau hinh

--- shop_backend/app/config/index.js
+++ shop_backend/app/config/index.js
@@ -5,6 +5,10 @@ const config = {
   db: {
     uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Shop",
   },
+  key: {
+    secretKey: process.env.secretKey || "h9pX213459903762!45New234",
+    geminiKey: process.env.GEMINI_API_KEY,
+  },
 };
 
 module.exports = config;
