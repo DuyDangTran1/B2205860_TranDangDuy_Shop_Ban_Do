c8512664aa95239c8cf3aac835fd87c6d2ad4735
DangDuyTran1 <tuechst1@gmail.com>
2026-03-20 11:54:02 +0700

    File chua cac ham tuong tac voi csdl Employee

---
+++ shop_backend/app/services/employee.service.js
@@ -0,0 +1,76 @@
+const { ObjectId } = require("mongodb");
+
+class Employee {
+  constructor(client) {
+    this.Employee = client.db().collection("Employee");
+  }
+  //hàm lọc dữ liệu nhân viên
+  extractEmployeeData(payload) {
+    const employee = {
+      name: payload.name,
+      email: payload.email,
+      phone: payload.phone,
+      password: payload.password,
+      address: payload.address,
+      role: payload.role || "nhân viên",
+      birthday: payload.birthday,
+      url_image: payload.url_image,
+      updated_at: new Date(),
+    };
+
+    Object.keys(employee).forEach(
+      (key) => employee[key] === undefined && delete employee[key],
+    );
+
+    return employee;
+  }
+  //Hàm tạo nhân viên
+  async createEmployee(payload) {
+    const newEmployee = this.extractEmployeeData(payload);
+    newEmployee.block = false;
+    newEmployee.created_at = new Date();
+    return await this.Employee.insertOne(newEmployee);
+  }
+  //Hàm cập nhật thông tin nhân viên
+  async updateStaff(employee_id, payload) {
+    const update = this.extractEmployeeData(payload);
+    return await this.Employee.findOneAndUpdate(
+      {
+        _id: ObjectId.isValid(employee_id) ? new ObjectId(employee_id) : null,
+      },
+      {
+        $set: update,
+      },
+      {
+        returnDocument: "after",
+      },
+    );
+  }
+  //Hàm chuyển đổi trạng thái hoạt động của nhân viên
+  async statusAccount(employee_id, status) {
+    return await this.Employee.findOneAndUpdate(
+      { _id: ObjectId.isValid(employee_id) ? new ObjectId(employee_id) : null },
+      { $set: { block: status } },
+      { returnDocument: "after" },
+    );
+  }
+
+  //Tìm nhân viên
+  async findById(id) {
+    return await this.Employee.findOne({
+      _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+    });
+  }
+
+  //Tìm nhân viên theo 1 trường nào đó
+  async findEmployee(filter) {
+    return await this.Employee.findOne(filter);
+  }
+
+  //Lấy tất cả nhân viên
+  async getListEmployee() {
+    return await this.Employee.find({ role: "Nhân viên" }).toArray();
+  }
+}
+
+module.exports = Employee;
