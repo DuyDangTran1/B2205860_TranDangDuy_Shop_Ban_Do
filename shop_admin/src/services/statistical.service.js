911dad4cb8633fcf3b7f796ec19d1aa305be3822
DangDuyTran1 <tuechst1@gmail.com>
2026-08-10 13:57:47 +0700

    Cap nhat api thong ke

--- shop_admin/src/services/statistical.service.js
+++ shop_admin/src/services/statistical.service.js
@@ -8,6 +8,13 @@ class Statistical {
   getDashboardStats = async (queryData) => {
     return (await this.Api.get("/", { params: queryData })).data;
   };
+  getInventoryStats = async (keyword = "") => {
+    return (await this.Api.get("/getInventoryStats", { params: { keyword } }))
+      .data;
+  };
+  getInventoryWarehouseStats = async (queryData) => {
+    return (await this.Api.get("/getInventoryWarehouseStats")).data;
+  };
 }
 
 export default new Statistical();
